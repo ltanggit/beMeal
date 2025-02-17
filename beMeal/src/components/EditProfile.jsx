70e5774176b6e1ef1c9e144c2ee24061b7fa6a38
lilliangonick <lillygonick@gmail.com>
2025-02-16 19:16:52 -0800

    created basic layout for profile page, created EditProfile component to handle editing profile information in a populated screen

---
+++ beMeal/src/components/EditProfile.jsx
@@ -0,0 +1,19 @@
+export function EditProfile() {
+    return (
+        <div className="edit-profile-box">
+            <h2>edit profile</h2>
+
+            <label>username</label>
+            <input/>
+
+            <label>bio</label>
+            <textarea/>
+
+            <label>email</label>
+            <input/>
+
+            <label>profile picture</label>
+            <input/>
+        </div>
+    )
+}
--- beMeal/src/pages/Profile.jsx
+++ beMeal/src/pages/Profile.jsx
@@ -1,10 +1,55 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { EditProfile } from "../components/EditProfile"
 
 export default function Profile() {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  }
+
+  if (isEditing) {
+    return (
+      <div>
+      <EditProfile/>
+    </div>
+    )
+  }
+  
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <p> Profile Page </p>
+      {/*profile header*/}
+      <div className="profile-header">
+        <img 
+          src="https://www.gravatar.com/avatar/?d=mp"
+          alt="profile-picture"
+        />
+        <div>
+          <h1>username</h1>
+          <p> user bio </p>
+        </div>
+        <button
+          onClick={handleEditClick}>
+          edit
+        </button>
+      </div>
+
+      {/*stats section*/}
+      <div className="stats-section">
+        <div>
+          <p>posts</p>
+        </div>
+        <div>
+          <p>followers</p>
+        </div>
+        <div>
+          <p>following</p>
+        </div>
+      </div>
+
+      {/*gallery*/}
     </div>
   );
 }
