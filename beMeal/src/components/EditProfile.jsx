73662d0529d09e837f5881612640a99bcaf92029
lilliangonick <lillygonick@gmail.com>
2025-02-23 17:07:30 -0800

    generated basic profile and edit profile page components

--- beMeal/src/components/EditProfile.jsx
+++ beMeal/src/components/EditProfile.jsx
@@ -1,19 +1,20 @@
 export function EditProfile() {
     return (
         <div className="edit-profile-box">
-            <h2>edit profile</h2>
+            <h1>edit profile</h1>
+            <div className="edit-profile-info">
+                <label>username</label>
+                <input/>
 
-            <label>username</label>
-            <input/>
+                <label>bio</label>
+                <textarea/>
 
-            <label>bio</label>
-            <textarea/>
+                <label>email</label>
+                <input/>
 
-            <label>email</label>
-            <input/>
-
-            <label>profile picture</label>
-            <input/>
+                <label>profile picture</label>
+                <input/>
+            </div>
         </div>
     )
 }
--- beMeal/src/pages/Profile.jsx
+++ beMeal/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { EditProfile } from "../components/EditProfile"
 
+
 export default function Profile() {
   const [isEditing, setIsEditing] = useState(false);
 
@@ -9,45 +10,77 @@ export default function Profile() {
     setIsEditing(true);
   }
 
-  if (isEditing) {
-    return (
-      <div>
-      <EditProfile/>
-    </div>
-    )
+  const handleCloseClick = () => {
+    setIsEditing(false); 
   }
+
+  // if (isEditing) {
+  //   return (
+  //     <div>
+  //     <button
+  //       onClick={handleCloseClick}>
+  //       x
+  //     </button>
+  //     <EditProfile/>
+  //   </div>
+  //   )
+  // }
   
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="profile-container">
+      <h1>welcome 'first-name'</h1>
       {/*profile header*/}
       <div className="profile-header">
         <img 
+          className="profile-picture"
           src="https://www.gravatar.com/avatar/?d=mp"
           alt="profile-picture"
         />
-        <div>
-          <h1>username</h1>
-          <p> user bio </p>
+
+        <div className="profile-info">
+          <h2>username</h2>
+          <p>bio</p>
         </div>
+
+        {/*stats section*/}
+        <div className="stats-section">
+          <div>
+            <p>posts: </p>
+          </div>
+          <div>
+            <p>followers: </p>
+          </div>
+          <div>
+            <p>following: </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="button-container">
         <button
           onClick={handleEditClick}>
           edit
         </button>
       </div>
 
-      {/*stats section*/}
-      <div className="stats-section">
-        <div>
-          <p>posts</p>
+      {isEditing && (
+        <div className="edit-popup">
+          <div className="edit-profile-box">
+            <EditProfile />
+          </div>
+
+          <button 
+            className="close-button" 
+            onClick={handleCloseClick}>
+              cancel
+            </button>
+            <button
+            className="save">
+              save
+            </button>
         </div>
-        <div>
-          <p>followers</p>
-        </div>
-        <div>
-          <p>following</p>
-        </div>
-      </div>
+      )}
 
       {/*gallery*/}
     </div>
