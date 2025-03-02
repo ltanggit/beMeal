9d7a42a928cd58cc3c0fc7dfa960eeb3b406801c
Kayla <kaylahamakawa@g.ucla.edu>
2025-03-02 13:40:36 -0800

    added initial comments endpoints and updated schema

--- beMeal/Server/Posts/postSchema.js
+++ beMeal/Server/Posts/postSchema.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 
+// const commentSchema = new mongoose.Schema({
+//   //check if we need commentId, there is _id
+//   commentId: { type: mongoose.Schema.Types.ObjectId, auto: true },
+//   content: { type: String, required: true },
+//   username: { type: String, required: true },
+//   timePosted: { type: Date, default: Date.now },
+// });
+
 const postSchema = new mongoose.Schema({
   username: { type: String, required: true },
   image: { type: String, required: true },
   caption: { type: String },
   timePosted: { type: Date, default: Date.now },
   likeCount: { type: Number, default: 0 },
+  comments: [
+    {
+      content: { type: String, required: true },
+      username: { type: String, required: true },
+      timePosted: { type: Date, default: Date.now },
+    },
+  ],
 });
 
 const Post = mongoose.model("Post", postSchema);
