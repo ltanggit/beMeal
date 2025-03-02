162caa2358063f66bd45d9505c295bd4cc8e2fa8
ltanggit <sendtolorelei@outlook.com>
2025-03-02 15:37:21 -0800

    added specification on the password for authentication

--- beMeal/Server/Authentication/registerAccount.js
+++ beMeal/Server/Authentication/registerAccount.js
@@ -10,6 +10,63 @@ export const registerAccount = async (req, res) => {
     if (existingAccount) {
       return res.status(400).json({ error: "Username already taken" });
     }
+    const existingID = await Account.findOne({ userID });
+    if (existingID) {
+      return res.status(400).json({ error: "User ID already taken" });
+    }
+
+    // Check if userName is valid
+    if (userName.length < 3) {
+      return res.status(400).json({ error: "User ID must be at least 3 characters" });
+    }
+
+    // Check if password is valid
+    if (password.length < 6) {
+      return res.status(400).json({ error: "Password must be at least 6 characters" });
+    }
+    // // Check if password has uppercase letter
+    // if (!/[A-Z]/.test(password)) {
+    //   return res.status(400).json({ error: "Password must contain at least one uppercase letter" });
+    // }
+    // // Check if password has lowercase letter
+    // if (!/[a-z]/.test(password)) {
+    //   return res.status(400).json({ error: "Password must contain at least one lowercase letter" });
+    // }
+    // // Check if password has number
+    // if (!/[0-9]/.test(password)) {
+    //   return res.status(400).json({ error: "Password must contain at least one number" });
+    // }
+    // // Check if password has special character
+    // if(!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+    //   return res.status(400).json({ error: "Password must contain at least one special character" });
+    // }
+
+    // if (!/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])/.test(password)) {
+    //     return res.status(400).json({ 
+    //         error: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character" 
+    //     });
+    //     }
+      
+
+    const errors = [];
+
+    if (!/[A-Z]/.test(password)) {
+    errors.push("at least one uppercase letter");
+    }
+    if (!/[a-z]/.test(password)) {
+    errors.push("at least one lowercase letter");
+    }
+    if (!/[0-9]/.test(password)) {
+    errors.push("at least one number");
+    }
+    if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+    errors.push("at least one special character");
+    }
+
+    if (errors.length) {
+    return res.status(400).json({ error: `Password must contain ${errors.join(", ")}` });
+    }
+
 
     // Check if a user profile exists for the userID
     // const existingUser = await User.findOne({ userID });
