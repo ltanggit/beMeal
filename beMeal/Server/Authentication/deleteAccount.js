94ef1306f37568d910ade7c2d7984176904b8a7b
ltanggit <sendtolorelei@outlook.com>
2025-03-03 14:59:23 -0800

    minor change to deleteAccount.js

---
+++ beMeal/Server/Authentication/deleteAccount.js
@@ -0,0 +1,23 @@
+import Account from "./accountSchema.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const deleteAccount = async (req, res) => {
+  try {
+    const { userName } = req.body;
+
+    // Find account by userName
+    const account = await Account.findOne({ userName });
+    // if account does not exist, return error
+    if (!account) {
+      return res.status(400).json({ error: "Account does not exist" });
+    }
+
+    await Account.deleteOne({ userName });
+    res.status(200).json({ message: "Account deleted" }); 
+
+    } catch (error) {
+      console.error("Error deleting account:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+};
