45a8db0c611546d87014ec03d022828f9aecf131
ltanggit <sendtolorelei@outlook.com>
2025-03-04 22:15:56 -0800

    updated authentication/account and user interaction in the database
    removed "userID" field from accountSchema and related functions
    stored account mongoDB._id in "userID" field in user.schema
    linked creation of authentication account registration with user metadata creation

--- beMeal/Server/Routes/userRoutes.js
+++ beMeal/Server/Routes/userRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { createUser } from '../Users/createUser.js';
 import { updateUser } from '../Users/updateUser.js';
 import { getFollowing } from '../Users/getFollowing.js';
 import { follow } from '../Users/updateFollowing.js';
@@ -8,7 +7,6 @@ import { getUser } from '../Users/getUser.js';
 
 const router = express.Router();
 
-router.post('/createUser', createUser); //the post request calls the createUser function
 router.put('/updateUser', updateUser); //create the putt for updating a user
 router.get('/getFollowing', getFollowing); //create the get request to get followers
 router.put('/follow', follow); //create put for following a user 
