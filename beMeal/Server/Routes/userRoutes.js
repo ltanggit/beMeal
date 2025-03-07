284de483ac37f8dbcaf4eb95c225f7b063f42b99
Ava Gonick <144552066+avagonick@users.noreply.github.com>
2025-03-07 14:26:21 -0800

    Adding ability to search a user

--- beMeal/Server/Routes/userRoutes.js
+++ beMeal/Server/Routes/userRoutes.js
@@ -4,6 +4,7 @@ import { getFollowing } from '../Users/getFollowing.js';
 import { follow } from '../Users/updateFollowing.js';
 import { unfollow } from '../Users/updateFollowing.js';
 import { getUser } from '../Users/getUser.js';
+import { searchUsers } from '../Users/searchUsers.js';
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.get('/getFollowing', getFollowing); //create the get request to get follo
 router.put('/follow', follow); //create put for following a user 
 router.put('/unfollow', unfollow); //create put for unfollowing a user
 router.get('/getUser', getUser); //create get to get users information
+router.get('/searchUsers', searchUsers); //create get to ge all the matching userNames
 
 
 export default router;
---
+++ beMeal/Server/Users/searchUsers.js
@@ -0,0 +1,25 @@
+import User from './userSchema.js';
+
+export const searchUsers = async (req, res) => {
+
+    try{
+    const userSearch = req.body.search; //grab the actual user being searched for 
+    const userID = req.body.userID; //your userID to check if you are following them 
+    const matchingUsers = await User.find({'userName': {$regex: userSearch}}, {'_id': 0, 'userName': 1, 'profilePic': 1, 'followers': 1})
+
+    //use matchingUsers to return an array that is just userName, profilePic, and isFollowing
+    //map function returns an array with a specific transformation to each entry
+    //this transformation is to create new json
+    const matchUsersWithIfFollowing = matchingUsers.map(user => (
+        {
+            userName: user.userName,
+            profilePic: user.profilePic, 
+            following: user.followers.includes(userID)
+        })
+    );
+    res.status(200).json({'users': matchUsersWithIfFollowing});
+    } catch (error){
+        console.error("Error finding matching users:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
