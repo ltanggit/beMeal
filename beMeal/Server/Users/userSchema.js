c7b8c5e68e643e8006b1e75ab33e3228510abf03
Kayla <kaylahamakawa@g.ucla.edu>
2025-03-09 23:35:59 -0700

    temp add posts array and last posted

--- beMeal/Server/Users/userSchema.js
+++ beMeal/Server/Users/userSchema.js
@@ -4,14 +4,16 @@ const Schema = mongoose.Schema; //extracting Schema from mongoose making it easi
 
 const userSchema = new Schema({
   userID: { type: mongoose.Schema.Types.ObjectID, required: true, index: true },
-  userName: { type: String, required: true, unique: true},
+  userName: { type: String, required: true, unique: true },
   bio: { type: String, maxLength: 1000, default: "" },
   profilePic: { type: Buffer, default: null },
   numFollowers: { type: Number, default: 0 },
   numFollowing: { type: Number, default: 0 },
-  followers: {type: [String], default : []},
-  following: {type: [String], default: []},
-  streakCount: { type: Number, default: 0 }
+  followers: { type: [String], default: [] },
+  following: { type: [String], default: [] },
+  streakCount: { type: Number, default: 0 },
+  posts: { type: [mongoose.Schema.Types.ObjectID], default: [] },
+  lastPostDate: { type: Date, default: null },
 });
 
 //will automatically use the connection without needing to reconnect
